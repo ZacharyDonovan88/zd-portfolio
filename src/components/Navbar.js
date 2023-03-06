068060a8f4d59bed7be5a874224d62cab2dc6aba
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-03-06 18:45:10 +1030

    updated style, added mobile features

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,36 +7,41 @@ function Home() {
    return (
      <>
 <div className="navDiv">
-    <h2>Zachary Donovan Ceruto</h2>
         <nav>
-            <ul>
-                
-                <li>
-                <NavLink exact to="/Home" activeClassName="active">
-                    Home
-                </NavLink>
-                </li>
-                <li>
-                <NavLink exact to="/About" activeClassName="active">
-                    About
-                </NavLink>
-                </li>
-                <li>
-                <NavLink exact to="/Work" activeClassName="active">
-                    Work
-                </NavLink>
-                </li>
-                <li>
-                <NavLink exact to="/Contact" activeClassName="active">
-                    Contact
-                </NavLink>
-                </li>
-                <li>
-                <NavLink exact to="/Resume" activeClassName="active">
-                    Resume
-                </NavLink>
-                </li>
-            </ul>
+            <div className="navName">
+                <h2>Zachary Donovan Ceruto</h2>
+            </div>
+            <div className="navPort">
+                <h2 id="portTitle">Portfolio</h2>
+            </div>
+                <ul>
+                    
+                    <li>
+                    <NavLink exact to="/Home" activeClassName="active">
+                        Home
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/About" activeClassName="active">
+                        About
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/Work" activeClassName="active">
+                        Work
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/Contact" activeClassName="active">
+                        Contact
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/Resume" activeClassName="active">
+                        Resume
+                    </NavLink>
+                    </li>
+                </ul>
         </nav>
 </div>
 
