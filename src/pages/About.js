1b93e17d271c810246ae60532e6a89d6d89248af
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-05-09 16:08:44 +0930

    updated style to about

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,14 +5,14 @@ function About() {
  
    return (
      <>
-        <div className="">
+        <div className="aboutStyle">
                 <h1>About</h1>
                 <p>
                     My name is Zachary Donovan Ceruto, I have just finished my full-stake 24 week course at Coding Boot Camp University of Adelaide.
                     I am very passionate about coding and my goal is to be part of a successful web developing team. 
                 </p>
                 <p>
-                    I am actively searching for a position as a junior web developer. 
+                    I am actively searching for a position as a junior web developer.  
                 </p>
                 <p>
                     I am actively searching for a position as a junior web developer. 
