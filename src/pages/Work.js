b5679a999710fe37b8e9027b06dd1a4d1ddf05fc
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-06-22 17:49:07 +0930

    added work page

---
+++ src/pages/Work.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+import AnimatedPage from "../AnimatedPage";
+
+function Work() {
+ 
+   return (
+     <AnimatedPage>
+        <div className="pageBox">
+          <h2 className="pageTitles">Work</h2>
+ 
+        </div>
+     </AnimatedPage>
+   );
+ }
+ 
+ export default Work;
