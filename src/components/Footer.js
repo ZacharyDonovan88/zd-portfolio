dcec81bd1a80c3ca07238dc030247b7329fdc57b
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-07-11 23:29:26 +0930

    updated footer and resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import "../App.css";
 const Footer = () => {
   
     return (
-      <>
+      <div className="footer-Container">
         <div className="footer">
             <div>
-                <h4 id="footerPhone">Phone Number: <a href="0422 159 001">0422 159 001</a></h4>
-                <h4 id="footerEmail">Email: <a href="Zachary.Donovan99@outlook.com">Zachary.Donovan99@outlook.com</a></h4>
+                <h4 id="footerPhone">Phone Number:<a href="0422 159 001">0422 159 001</a></h4>
+                <h4 id="footerEmail">Email:<a href="Zachary.Donovan99@outlook.com">Zachary.Donovan99@outlook.com</a></h4>
             </div>
         </div>
-      </>
+      </div>
     );
   };
   
